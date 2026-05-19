462828abf6eed945d7b3e624853ac169cdf628d2
yudeli3721 <yudeli3721@gmail.com>
2026-05-19 20:10:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,23 +26,3 @@ with st.popover("快速進階篩選"):
     st.checkbox("隱藏已過期行程")
 
 st.columns(2)
-
-    my_color = st.color_picker(
-     "挑選辨識顏色",
-     "#1A73E8"
-    )
-    
-    meeting_time = st.time_input(
-      "選擇時間"
-    )
-    
-    import datetime
-    today = st.date_input(
-      "選擇日期",
-      datetime.date.today()
-    )
-    
-    title = st.text_input(
-      "行程主旨",
-      placeholder="請填寫會議名稱..."
-    )
