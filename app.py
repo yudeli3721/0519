5f017cd3c97b9046b1fd34ba9c493f589a5e8b0e
yudeli3721 <yudeli3721@gmail.com>
2026-05-19 19:23:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,23 +4,18 @@ with st.sidebar:
     st.write("###  行事曆群組")
     st.radio("選擇群組", ["工作", "家庭", "朋友"])
 
-col_left, col_center, col_right = st.columns([1, 2, 1.2], gap="large")
+col_left, col_right = st.columns([1, 3], gap="large")
 
 with col_left: 
     st.write("###  新增區") 
     st.button("按鈕放左邊")
 
-with col_center: 
-    st.write("###  看板區") 
-    st.info("主要行程訊息放中間")
-
 with col_right: 
     st.write("###  設定區") 
     st.button("控制項放右邊")
-
-with st.container(border=True): 
-    st.write(" 標題：開學典禮") 
-    st.write(" 時間：09:00")
+    with st.container(border=True): 
+        st.write(" 標題：開學典禮") 
+        st.write(" 時間：09:00")
 
 with st.expander("查看進階提醒參數設定"):
     st.write("這裡是發信伺服器的底層設定...")
