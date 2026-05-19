2f9d383845ace049ad7fecefcaf8a26a7e7120d6
yudeli3721 <yudeli3721@gmail.com>
2026-05-19 19:08:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,18 @@ import streamlit as st
 st.set_page_config(page_title="微型 TimeTree", layout="wide")
 with st.sidebar:
     st.write("###  行事曆群組")
-    st.radio("選擇群組", ["工作", "家庭"])
+    st.radio("選擇群組", ["工作", "家庭", "朋友"])
+
+col_left, col_center, col_right = st.columns([1, 2, 1.2], gap="large")
+
+with col_left: 
+    st.write("###  新增區") 
+    st.button("按鈕放左邊")
+
+with col_center: 
+    st.write("###  看板區") 
+    st.info("主要行程訊息放中間")
+
+with col_right: 
+    st.write("###  設定區") 
+    st.button("控制項放右邊")
