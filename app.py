4e70daec17bc65a68a6e8fe8672fcae6270ca1bf
yudeli3721 <yudeli3721@gmail.com>
2026-05-19 19:25:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="微型 TimeTree", layout="wide")
+
 with st.sidebar:
     st.write("###  行事曆群組")
     st.radio("選擇群組", ["工作", "家庭", "朋友"])
@@ -20,7 +21,4 @@ with col_right:
 with st.expander("查看進階提醒參數設定"):
     st.write("這裡是發信伺服器的底層設定...")
 
-@st.dialog("系統公告")
-def show_alert():
-    st.write("本週作業請確認 requirements.txt 有正確設定！")
-if st.button("查看公告"): show_alert()
+with st.tabs("本月行程", "已封存行程")
